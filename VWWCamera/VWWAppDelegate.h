1101ebf61c06bc98e72a8a96948e01e5ae405082
Zakk Hoyt <vaporwarewolf@gmail.com>
2014-04-05 09:15:52 -0700

    Initial Commit

---
+++ VWWCamera/VWWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWWAppDelegate.h
+//  VWWCamera
+//
+//  Created by Zakk Hoyt on 4/5/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VWWCameraTests/VWWCameraTests.m
@@ -0,0 +1,34 @@
+//
+//  VWWCameraTests.m
+//  VWWCameraTests
+//
+//  Created by Zakk Hoyt on 4/5/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VWWCameraTests : XCTestCase
+
+@end
+
+@implementation VWWCameraTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
