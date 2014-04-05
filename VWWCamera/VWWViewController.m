616522608aeabf5d20e8d759e683fae12c5aae0a
Zakk Hoyt <vaporwarewolf@gmail.com>
2014-04-05 09:32:52 -0700

    Initial checkin

--- VWWCamera/VWWViewController.m
+++ VWWCamera/VWWViewController.m
@@ -7,13 +7,22 @@
 //
 
 #import "VWWViewController.h"
+#import "GPUImage.h"
+#import "GPUImageiOSBlurFilter.h"
 
-@interface VWWViewController ()
-
+@interface VWWViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIActionSheetDelegate>
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *albumButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *cameraButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *filterButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
+@property (weak, nonatomic) IBOutlet UIImageView *selectedImageView;
+@property (nonatomic, strong) UIImage *originalImage;
 @end
 
 @implementation VWWViewController
 
+#pragma mark UIViewController
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -25,5 +34,121 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark Private methods
 
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    NSString *alertTitle;
+    NSString *alertMessage;
+    
+    if(!error)
+    {
+        alertTitle   = @"Image Saved";
+        alertMessage = @"Image saved to photo album successfully.";
+    }
+    else
+    {
+        alertTitle   = @"Error";
+        alertMessage = @"Unable to save to photo album.";
+    }
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:alertTitle
+                                                    message:alertMessage
+                                                   delegate:self
+                                          cancelButtonTitle:@"Okay"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+#pragma mark IBActions
+- (IBAction)albumButtonTouchUpInside:(id)sender {
+    UIImagePickerController *photoPicker = [[UIImagePickerController alloc] init];
+    photoPicker.delegate = self;
+    photoPicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:photoPicker animated:YES completion:NULL];
+    
+}
+- (IBAction)cameraButtonTouchUpInside:(id)sender {
+    UIImagePickerController *photoPicker = [[UIImagePickerController alloc] init];
+    
+    photoPicker.delegate = self;
+    photoPicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:photoPicker animated:YES completion:NULL];
+    
+}
+
+- (IBAction)filterButtonTouchUpInside:(id)sender {
+    UIActionSheet *filterActionSheet = [[UIActionSheet alloc] initWithTitle:@"Select Filter"
+                                                                   delegate:self
+                                                          cancelButtonTitle:@"Cancel"
+                                                     destructiveButtonTitle:nil
+                                                          otherButtonTitles:@"Grayscale", @"Sepia", @"Sketch", @"Pixellate", @"Color Invert", @"Toon", @"Pinch Distort", @"Blur", @"None", nil];
+    
+    [filterActionSheet showFromBarButtonItem:sender animated:YES];
+
+}
+- (IBAction)saveButtonTouchUpInside:(id)sender {
+    UIImageWriteToSavedPhotosAlbum(self.selectedImageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+
+}
+
+#pragma mark UIImagePickerDelegate
+- (void)imagePickerController:(UIImagePickerController *)photoPicker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    self.saveButton.enabled = YES;
+    self.filterButton.enabled = YES;
+    
+    self.originalImage = [info valueForKey:UIImagePickerControllerOriginalImage];
+    
+    [self.selectedImageView setImage:self.originalImage];
+    
+    [photoPicker dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+
+#pragma mark UIActionSheetDelegate
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    GPUImageFilter *selectedFilter;
+    switch (buttonIndex) {
+        case 0:
+            selectedFilter = [[GPUImageGrayscaleFilter alloc] init];
+            break;
+        case 1:
+            selectedFilter = [[GPUImageSepiaFilter alloc] init];
+            break;
+        case 2:
+            selectedFilter = [[GPUImageSketchFilter alloc] init];
+            break;
+        case 3:
+            selectedFilter = [[GPUImagePixellateFilter alloc] init];
+            break;
+        case 4:
+            selectedFilter = [[GPUImageColorInvertFilter alloc] init];
+            break;
+        case 5:
+            selectedFilter = [[GPUImageToonFilter alloc] init];
+            break;
+        case 6:
+            selectedFilter = [[GPUImagePinchDistortionFilter alloc] init];
+            break;
+        case 7:{
+            GPUImageiOSBlurFilter *blurFilter = [[GPUImageiOSBlurFilter alloc]init];
+            selectedFilter = (GPUImageFilter*)blurFilter;
+        }
+            break;
+        case 8:
+            selectedFilter = [[GPUImageFilter alloc] init];
+            break;
+        default:
+            break;
+    }
+    
+    UIImage *filteredImage = [selectedFilter imageByFilteringImage:self.originalImage];
+    [self.selectedImageView setImage:filteredImage];
+}
 @end
